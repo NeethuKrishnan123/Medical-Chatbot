3ec92a3776d423b202d047b32d1fd6e2a6feac8e
Official <neethukrishnan020@gmail.com>
2026-07-15 12:05:39 +0530

    Web app completed

---
+++ store_index.py
@@ -0,0 +1,46 @@
+from dotenv import load_dotenv
+import os
+from src.helper import load_pdf_file,filter_to_minimal_docs,text_split,download_hugging_face_embedding
+from pinecone import Pinecone
+from pinecone import ServerlessSpec
+from langchain_pinecone import PineconeSparseVectorStore
+
+load_dotenv()
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["GROQ_API_KEY"] = GROQ_API_KEY
+
+extracted_data = load_pdf_file('data/')
+filter_data = filter_to_minimal_docs(extracted_data)
+texts_chunk = text_split(filter_data)
+
+embedding = download_hugging_face_embedding()
+
+pinecone_api_key = PINECONE_API_KEY
+pc = Pinecone(api_key = pinecone_api_key)
+
+
+
+index_name = "medical-chatbot"
+
+if not pc.has_index(index_name):
+    pc.create_index(
+        name = index_name,
+        dimension=384, # Dimensions of the embedding
+        metric = "cosine", # Cosine similarity
+        spec = ServerlessSpec(cloud="aws", region="us-east-1")
+    )
+
+index = pc.Index(index_name)
+
+
+from langchain_pinecone import PineconeVectorStore
+
+docsearch = PineconeVectorStore.from_documents(
+    documents = texts_chunk,
+    embedding = embedding,
+    index_name = index_name
+)
